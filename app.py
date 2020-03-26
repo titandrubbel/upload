4bc182a3beb4733196fe17699d474c18942ab0d9
Rutger Drubbel <55639101+titandrubbel@users.noreply.github.com>
2020-03-26 15:56:31 +0100

    Delete app.py

--- app.py
+++
@@ -1,6 +0,0 @@
-UPLOAD_FOLDER = ''${CHE_PROJECTS_ROOT}/test''
-
-app = Flask(__name__)
-app.secret_key = "secret key"
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
